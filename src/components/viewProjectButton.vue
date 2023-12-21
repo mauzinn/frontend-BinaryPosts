6e6f4a39c2706458fa32ac7d5f2f4fff15801c07
mauzinn <ricioriciiorufino2008@gmail.com>
2023-12-21 18:21:26 -0300

    deploy

--- src/components/viewProjectButton.vue
+++ src/components/viewProjectButton.vue
@@ -2,7 +2,7 @@
     <section>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 
-        <a href="#">
+        <a href="https://github.com/mauzinn/frontend-BinaryPosts">
             <abbr title="Repository">
                 <button class="project">
                     <span class="material-symbols-outlined">
